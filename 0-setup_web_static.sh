bbca7ac89cd296d642618c04c533ee7a7afdc0c2
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-10 20:08:42 +0100

    add file creation execution if nginx is not installed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,17 @@ then
 	sudo apt update
 	sudo apt install -y nginx
 	sudo service nginx start
+	[ -d /data/web_static/releases/test ] || sudo mkdir -p /data/web_static/releases/test
+	[ -d /data/web_static/shared ] || sudo mkdir -p /data/web_static/shared
+	if [ -L /data/web_static/current ]; 
+	then 
+		sudo rm /data/web_static/current
+		sudo ln -s /data/web_static/releases/test /data/web_static/current
+	else
+		sudo ln -s /data/web_static/releases/test /data/web_static/current
+	fi
+	echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+	sudo chown -R ubuntu:ubuntu /data/
 else
 	[ -d /data/web_static/releases/test ] || sudo mkdir -p /data/web_static/releases/test
 	[ -d /data/web_static/shared ] || sudo mkdir -p /data/web_static/shared
@@ -29,3 +40,4 @@ sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.ba
 # add the location /hbnb_static/ { } block to the default Nginx configuration
 export replacement="\tlocation /hbnb_static {\n\t\t alias /data/web_static/current \n\t}\n"
 sudo sed -ie '48a\ '"${replacement}"'' /etc/nginx/sites-available/default;
+sudo service nginx restart
