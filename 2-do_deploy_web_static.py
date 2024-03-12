251b95b20bbae200a403b68ee64a1fbe8f2c0506
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-12 11:05:17 +0100

    Changes made to new commit
    1. Made env.hosts and env.user global
    2. Corrected file path where tgz archive is to be placed
    3. Corrected archive name to be deleted

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,24 +2,28 @@
 
 """ Fabric script that distributes an archive to web servers """
 
-from fabric.api import local, run, sudo, put, env
+from fabric.api import *
+from fabric.contrib.console import confirm
 import os
+
+
+env.hosts = ['18.234.253.161', '34.224.4.240']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """ Distributes archive to web servers """
+
     if not os.path.exists(archive_path):
         return False
-    env.hosts = ['18.234.253.161', '34.224.4.240']
-    env.users = ["ubuntu"]
     try:
         put(archive_path, '/tmp/')
         f = archive_path.split('/')[-1].split('.')[0]
         sudo("mkdir -p /data/web_static/releases/{}/".format(f))
         """ directory  archive is to be extracted """
         dirs = "/data/web_static/releases/{}/".format(f)
-        sudo("tar -xzf /tmp/{} -C {}".format(archive_path, dirs))
-        sudo("rm /tmp/{}" .format(f))
+        sudo("tar -xvf /tmp/{}.tgz -C {}".format(f, dirs))
+        sudo("rm /tmp/{}.tgz" .format(f))
         sudo("rm /data/web_static/current")
         new_link = "/data/web_static/current"
         sudo("ln -sf /data/web_static/releases/{} {}".format(f, new_link))
