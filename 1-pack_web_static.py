445bf81259b41ceb5177a649230425f1cd25d423
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-12 01:13:53 +0100

    test tar command

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     """ Function generates .tgz archive """
     dates = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     local("mkdir versions")
-    cmd = local("tar -cvzf ./versions/web_static_{}.tgz ./web_static"
+    cmd = local("tar -cvzf versions/web_static_{}.tgz ./web_static"
                 .format(dates))
     if cmd.failed:
         return None
