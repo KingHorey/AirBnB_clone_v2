0866aacec40f25524a85caf49a8ec26db049d9dc
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-12 01:49:28 +0100

    add check for exsitence of versions dir

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 def do_pack():
     """ Function generates .tgz archive """
     dates = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    local("mkdir versions")
+    local(" [ -d versions ] || mkdir versions ")
     file_name = "versions/web_static_{}.tgz".format(dates)
     cmd = local("tar -cvzf {} ./web_static".format(file_name))
     if cmd.failed:
