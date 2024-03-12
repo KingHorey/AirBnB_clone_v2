671d32c5846b2712b256224e3ab959158301666e
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-12 03:27:01 +0100

    change uncompresses tar variable name

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,11 +11,12 @@ def do_deploy(archive_path):
     env.hosts = ['18.234.253.161', '34.224.4.240']
     try:
         put(archive_path, '/tmp/')
-        fil = archive_path.split('/')[1].split('.')[0]
-        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/".format(fil))
+        f = archive_path.split('/')[-1].split('.')[0]
+        sudo("tar -xzf /tmp/{} -C /data/web_static/releases/".format(f))
         sudo("rm /tmp/{}" .format(fil))
         sudo("rm /data/web_static/current")
-        sudo(f"ln -sf /data/web_static/releases/{fil} /data/web_static/current")
+        new_link = "/data/web_static/current"
+        sudo("ln -sf /data/web_static/releases/{} {}".format(f, new_link))
         return True
     except Exception:
         return False
