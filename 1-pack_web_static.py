72e2385962c15fda214c3cb8d9129825afa2783a
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-12 01:07:11 +0100

    make use of strftime to get required date, time

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,16 +7,10 @@ from datetime import datetime
 
 def do_pack():
     """ Function generates .tgz archive """
-    year = datetime.now().year
-    month = datetime.now().month
-    day = datetime.now().day
-    hour = datetime.now().hour
-    minute = datetime.now().minute
-    seconds = datetime.now().second
+    dates = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     local("mkdir versions")
-    time_stamp = "{}{}{}{}{}{}".format(year, month, day, hour, minute, seconds)
     cmd = local("tar -cvzf ./versions/web_static_{}.tgz ./web_static"
-                .format(time_stamp), capture=True)
+                .format(dates), capture=True)
     if cmd.failed:
         return None
     else:
