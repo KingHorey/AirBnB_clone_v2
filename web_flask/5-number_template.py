10da1c058cdeb6294c311e00e570a683559fdc86
kingHorey <oreoluwaakinbo.oa@gmail.com>
2024-03-24 13:40:09 +0100

    render template with parameter

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" import flask """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """ home route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_page():
+    """ hbnb route """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_page(text):
+    """ return c page """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>")
+@app.route("/python")
+def python_page(text=None):
+    """ return python page """
+    if text is None:
+        return "Python is cool"
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def get_number(n):
+    """ return number page """
+    if (isinstance(n, int)):
+        return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def return_number(n):
+    if (isinstance(n, int)):
+        return render_template("5-number.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run()
